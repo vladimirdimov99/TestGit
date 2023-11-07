c5f37e51a6ec34667a0abeadb5fa85af9ebeee75
Vladimir <vladimirdimov99@gmail.com>
2023-11-07 10:19:45 +0200

    Upload the project

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        double area = Math.PI * r * r;
+        System.out.println(area);
+    }
+}
